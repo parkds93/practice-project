17642480a886aece46258615658dfabe2661ba04
박대성 <8757279@naver.com>
2022-12-22 11:14:24 +0900

    INITIALIZE

---
+++ practice-project/src/main/java/com/practiceproject/PracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practiceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProjectApplication.class, args);
+	}
+
+}
